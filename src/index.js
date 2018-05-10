ff571ea6f7f7781ea500b8450a72ea5a3c7a419a
Vagios Vlachos <bagios234@gmail.com>
2018-05-10 10:01:48 +0300

    Add CSS preprocessor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
+import './styles/styles.css';
 
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
